716edd3f565a47a37754525bd910d49f8010ca8a
Robert <micu.robert24@gmail.com>
2025-02-22 02:54:23 +0200

    feat: sliding window technique LongestSubstringWithoutRepeatingCharacters

--- src/main/java/slidingwindow/LongestSubstringWithoutRepeatingCharacters.java
+++ src/main/java/slidingwindow/LongestSubstringWithoutRepeatingCharacters.java
@@ -1,2 +1,30 @@
-package slidingwindow;public class LongestSubstringWithoutRepeatingCharacters {
+package slidingwindow;
+
+import java.util.HashMap;
+import java.util.Map;
+
+// https://leetcode.com/problems/longest-substring-without-repeating-characters/description/
+public class LongestSubstringWithoutRepeatingCharacters {
+
+    public int lengthOfLongestSubstring(String s) {
+
+        // Use hasMap to store character and its las index
+        Map<Character, Integer> characters = new HashMap<>();
+        // final result
+        int result = 0;
+        // left pointer of substring
+        int left = 0;
+
+        for (int right = 0; right < s.length(); right++) {
+            char currentCharacter = s.charAt(right);
+            // if the character already exist and its index is in the window move left pointer to character index + 1
+            if (characters.containsKey(currentCharacter) && characters.get(currentCharacter) >= left) {
+                left = characters.get(currentCharacter) + 1;
+            }
+            characters.put(currentCharacter, right);
+            result = Math.max(result, right - left + 1);
+        }
+
+        return result;
+    }
 }
--- src/test/java/slidingwindow/LongestSubstringWithoutRepeatingCharactersTest.java
+++ src/test/java/slidingwindow/LongestSubstringWithoutRepeatingCharactersTest.java
@@ -1,2 +1,18 @@
-package slidingwindow;public class LongestSubstringWithoutRepeatingCharactersTest {
+package slidingwindow;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class LongestSubstringWithoutRepeatingCharactersTest {
+
+    @Test
+    public void testLengthOfLongestSubstring() {
+
+        LongestSubstringWithoutRepeatingCharacters solution = new LongestSubstringWithoutRepeatingCharacters();
+
+        assertEquals(3, solution.lengthOfLongestSubstring("abcabcbb"));
+        assertEquals(1, solution.lengthOfLongestSubstring("bbbbb"));
+        assertEquals(3, solution.lengthOfLongestSubstring("pwwkew"));
+    }
 }
