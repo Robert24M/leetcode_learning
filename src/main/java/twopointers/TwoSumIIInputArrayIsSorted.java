c9f10fd3b09753af9e11a4ee6fbd885d008299c6
Robert <mirob1179@gmail.com>
2025-04-05 14:48:40 +0300

    feat: sliding window practice, minSizeSubarraySum

--- src/main/java/twopointers/TwoSumIIInputArrayIsSorted.java
+++ src/main/java/twopointers/TwoSumIIInputArrayIsSorted.java
@@ -4,18 +4,34 @@ package twopointers;
 public class TwoSumIIInputArrayIsSorted {
 
     public int[] twoSum(int[] numbers, int target) {
-        int left = 0;
-        int right = numbers.length - 1;
-        while (left < right) {
-            int currentSum = numbers[left] + numbers[right];
-            if (currentSum < target) {
+
+        int left = 0, right = numbers.length - 1;       // start with a pointer from the start of the array and with another from the end
+        while (left != right) {                         // while the pointers do not intersect
+            if (numbers[left] + numbers[right] < target) {
                 left++;
-            } else if (currentSum > target) {
+            } else if (numbers[left] + numbers[right] > target) {
                 right--;
             } else {
                 return new int[]{left + 1, right + 1};
             }
         }
-        return null;
+        return new int[]{-1, -1};
+    }
+
+    public int[] twoSumAgain(int[] numbers, int target) {
+        int left = 0;
+        int right = numbers.length - 1;
+        while (left != right) {
+            int current = numbers[right] + numbers[left];
+            if (current == target) {
+                return new int[]{left + 1, right + 1};
+            } else if (target > current) {
+                left++;
+            } else {
+                right--;
+            }
+        }
+
+        return new int[]{0, 0};
     }
 }
